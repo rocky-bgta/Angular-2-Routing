b6c95b5baa723418d3d296adb6f5e12f0d4bc4ea
rocky-bgta <rocky.bgta@gmail.com>
2017-08-21 11:39:48 +0600

    Extracting Route paramater Using ActivatedRoute and subsribtion and (Obserable) concept to detect parameter value change.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnDestroy} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   moduleId: module.id,
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
-      <a routerLink="">Home</a>
       <button (click)="onNavigate()">Go Home</button>
+      <hr>
+      {{id}}
     `
 })
-export class UserComponent {
-    constructor(private router: Router){}
+export class UserComponent implements  OnDestroy{
+
+    id: string;
+    private subsciption: Subscription;
+
+    constructor(private router: Router, private activatedRoute: ActivatedRoute){
+        this.subsciption = activatedRoute.params.subscribe(
+            (param: any) => this.id = param['id']
+        );
+    }
     onNavigate(){
         this.router.navigate(['/']);
+    }
+
+    ngOnDestroy(): void {
+        this.subsciption.unsubscribe();
     }
 }
