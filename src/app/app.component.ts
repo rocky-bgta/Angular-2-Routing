d96bb83b6bc6aace254500e2127c355b76680a0c
rocky-bgta <rocky.bgta@gmail.com>
2017-08-21 15:15:44 +0600

    add style base on active path through routerLinkActiveOptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core'
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styles:[`
+    .active{
+        border: 1px solid red;
+    }
+  `]
 })
 export class AppComponent {
 }
